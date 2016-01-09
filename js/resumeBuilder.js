ad3b43b774ff1c1121bb97e58bc6c9e8d6baa963
mancoi <caoman96@gmail.com>
2016-01-09 19:18:08 +0700

    update resumeBuilder file to the last Quizz in Lesson 1, all the code of any Quizz before has been putted to rsbBackup_Before_Quizz file

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,33 +1,83 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
-/*var awesomeThoughts = "I am Man and I am AWESOME"
-var funThoughts = awesomeThoughts.replace("AWESOME", "FUN");
-
-console.log(awesomeThoughts);
-console.log(funThoughts);
-
-$("#main").append(funThoughts);*/
-
-/*var formattedName, formattedRole;
-formattedName = HTMLheaderName.replace("%data%", "PCM");
-formattedRole = HTMLheaderRole.replace("%data%", "Web Developer");
-$("#header").prepend(formattedRole);
-$("#header").prepend(formattedName);*/
-
-var name = "mAn coi";
-
-function nameChanger(oldName) {
-    var finalName = oldName;
-    // Your code goes here!
-    var name = oldName.split(' ');
-    name[0] = name[0].toLowerCase();
-    name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
-    name[1] = name[1].toUpperCase();
-    finalName = name.join(" ");
-    
-    // Don't delete this line!
-    return finalName;
-};
-
-$("#main").append(nameChanger(name));
+var work = {
+	"jobs" : [
+		{
+			"employer" : "No",
+			"title" : "No",
+			"location" : "No",
+			"dates" : "No",
+			"desciption" : "No"
+		},
+		{
+			"employer" : "No",
+			"title" : "No",
+			"location" : "No",
+			"dates" : "No",
+			"desciption" : "No"
+		}
+	]
+}
+var projects = {
+	"projects" : [
+		{
+			"title" : "No",
+			"dates" : "No",
+			"desciption" : "No",
+			"images" : ["images/none.jpg"]
+		},
+		{
+			"title" : "No",
+			"dates" : "No",
+			"desciption" : "No",
+			"images" : ["images/none.jpg"]
+		}
+	]
+}
+var bio = {
+	"name" : "Pham Cao Man",
+	"role" : "Student",
+	"welcomeMessage" : "Can I do something for you?",
+	"contacts" : {
+		"email" : "caoman6@gmail.com",
+		"mobile" : "8411111111",
+		"github" : "mancoi",
+		"location" : "VN"
+	},
+	"skills" : ["HTML5", "CSS3", "Javascript", "C++", "OOP"]
+}
+var education = {
+	"schools" : [
+		{
+			"name" : "Open University",
+			"location" : "HCMC",
+			"degree" : "Computer Scientist",
+			"major" : ["CS"],
+			"dates" : 2014,
+			"url" : "ou.edu.vn"
+		},
+				{
+			"name" : "Nguyen Chi Thanh High School",
+			"location" : "HCMC",
+			"degree" : "High School Graduated",
+			"major" : ["No"],
+			"dates" : 2014,
+			"url" : "No"
+		}
+	],
+	"onlinecourse" : [
+		{
+			"title" : "Javascript Basic",
+			"school" : "Udacity",
+			"dates" : 2016,
+			"url" : "https://www.udacity.com/course/javascript-basics--ud804"
+		},
+		{
+			"title" : "How to use Git and GitHub",
+			"school" : "Udacity",
+			"dates" : 2015,
+			"url" : "https://www.udacity.com/course/how-to-use-git-and-github--ud775"
+		}
+	]
+}
